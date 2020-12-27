28e2c65fbfe2b09928635a037a08c1318d9124ae
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2020-12-27 15:07:22 +0530

    🎉 Add other Image commands

---
+++ commands/image/affect.js
@@ -0,0 +1,28 @@
+const DIG = require("discord-image-generation");
+const Discord = require("discord.js");
+
+module.exports = {
+    config: {
+        name: 'affect',
+        description: 'this won\'t affect my baby!',
+        aliases: ["affect"],
+        usage: '',
+        accessableby: "",
+    },
+    run: async (client, message, args) => {
+ //   const m = client.findMember(message, args, true);
+   
+ let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+ let m = await message.channel.send("**Please Wait...**");   
+ let avatar = user.user.displayAvatarURL({
+      dynamic: false,
+      format: "png",
+    });
+
+    let img = await new DIG.Affect().getImage(avatar);
+
+    let attach = new Discord.MessageAttachment(img, "thomas.png");
+    m.delete({ timeout: 5000 });
+    message.channel.send(attach);
+  },
+};
