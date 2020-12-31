47197a2b4bf088a847db53a7ce4e5ea9977dee3e
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2020-12-31 18:02:48 +0530

    🐛 Fixed addemoji bug (Emoji Limit error)

--- commands/other/addemoji.js
+++ commands/other/addemoji.js
@@ -12,41 +12,44 @@ module.exports = {
         accessableby: "",
     },
     run: async (client, message, args) => {
-    
+
         if (!message.member.hasPermission(`MANAGE_EMOJIS`)) {
-            return message.channel.send(`${client.emotes.error} You Don't Have Permission To Use This Command! Manage Emojis`)
+            return message.channel.send(`${client.emotes.error} You don't have the permissions to use this command [Manage Emojis]!`)
         }
 
         const emoji = args[0];
-        if (!emoji) return message.channel.send(`${client.emotes.error} Please Give Me A Emoji!`);
+        if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
 
         let customemoji = Discord.Util.parseEmoji(emoji);
-
         if (customemoji.id) {
-            const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
-customemoji.animated ? "gif" : "png"
-}`;
+            const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${customemoji.animated ? "gif" : "png"
+                }`;
             const name = args.slice(1).join(" ");
-            message.guild.emojis.create(
+            try {
+                const Added = new MessageEmbed()
+                .setColor(config.embedcolor)
+                .setTitle(`${client.emotes.verified} Emoji Added`)
+                .setDescription(
+                `${client.emotes.verified} Emoji Has Been Added! | Name : ${name || `${customemoji.name}`} | Preview : [Click Me](${Link})`
+                    );
+                await message.guild.emojis.create(
                     `${Link}`,
                     `${name || `${customemoji.name}`}`
-);
-const Added = new MessageEmbed()
-.setColor(config.embedcolor)
-.setTitle(`${client.emotes.verified} Emoji Added`)
-.setDescription(
-`${client.emotes.verified} Emoji Has Been Added! | Name : ${name || `${customemoji.name}`} | Preview : [Click Me](${Link})`
-);
-return message.channel.send(Added);
-} else {
-let CheckEmoji = parse(emoji, { assetType: "png" });
-if (!CheckEmoji[0])
-return message.channel.send(`${client.emotes.error} Please Give Me A Valid Emoji!`);
-message.channel.send(
-`${client.emotes.error} You Can Use Normal Emoji Without Adding In Server!`
-);
-}
+                )
+                return message.channel.send(Added)
+            } catch (err) {
+                console.log(err)
+                return message.channel.send(`${client.emotes.error} An error has occured!\n\n**Possible Reasons:**\n\`\`\`- This server has reached the emojis limit\n- The bot doesn't have permissions.\n- The emojis size is too big.\`\`\``)
+           
+            }
+        } else {
+            let CheckEmoji = parse(emoji, { assetType: "png" });
+            if (!CheckEmoji[0])
+                return message.channel.send(`${client.emotes.error} **Please Give Me A Valid Emoji!**`);
+            message.channel.send(
+                `${client.emotes.error} **You Can Use Normal Emoji Without Adding In Server!**`
+            );
+        }
 
     }
 }
-
