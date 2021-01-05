7a1caf20db856c420374799f189c41cc554147ff
hanamispkz <hanamispkz@gmail.com>
2021-01-05 10:17:11 +0700

    ee

--- events/client/ready.js
+++ events/client/ready.js
@@ -11,7 +11,7 @@ module.exports = async client => {
     console.log(chalk.red`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} Logged In!`);
     function randomStatus() {
     let status = [`=help | ${client.guilds.cache.size} Server`,
-      `=help | ${client.users.cache.size} Members`];
+      `=help | ${totalUsers} Members`];
     let rstatus = Math.floor(Math.random() * status.length);
     client.user.setActivity(status[rstatus], { type: 'PLAYING' });
 
