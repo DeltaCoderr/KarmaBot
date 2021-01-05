b19c06559e28e904949bbb3f9ea93c5ec9ba67a9
DevRadd <75932400+DevRad0415@users.noreply.github.com>
2021-01-05 16:58:21 +0800

    Update ban.js

--- moderation/ban.js
+++ moderation/ban.js
@@ -16,7 +16,7 @@ module.exports = {
             const noargs = new MessageEmbed()
             .addField("Error!", "```diff\n- No user mentioned!```")
             .addField("Usage", `\`\`\`diff\n+ ${prefix}ban [user] (reason)\`\`\``)
-            .setColor("#FF0000")
+            .setColor(config.embedcolor)
             if (!args[0]) return message.channel.send(noargs) 
 
             let banMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
@@ -24,12 +24,12 @@ module.exports = {
             const nomember = new MessageEmbed()
             .addField("Error!", "```diff\n- Unable to find that user!```")
             .addField("Try", "```diff\n+ Checking if that user is in this server, and rerun the command again!```")
-            .setColor("#FF0000")
+            .setColor(config.embedcolor)
 
             const selfban = new MessageEmbed()
             .addField("Error!", "```diff\n- You can't ban yourself!```")
             .addField("Try", "```diff\n+ Mentioning someone again!```")
-            .setColor("#FF0000")
+            .setColor(config.embedcolor)
             if (!banMember) return message.channel.send(nomember);
             if (banMember === message.member) return message.channel.send(selfban)
 
@@ -37,7 +37,7 @@ module.exports = {
    const unbannable = new MessageEmbed()
             .addField("Error!", "```diff\n- Unable to ban that user!```")
             .addField("Try", "```diff\n+ Checking your role permission or make sure that user is not higher than yours!```")
-            .setColor("#FF0000")
+            .setColor(config.embedcolor)
 
             if (!banMember.bannable) return message.channel.send(unbannable)
             try {
@@ -46,7 +46,7 @@ module.exports = {
             .addField("Banned!", `\`\`\`diff\n- You got banned from ${message.guild.name}!\`\`\``)
             .addField("Reason", `\`\`\`diff\n+ ${reason || "None."}!\`\`\``)
             .addField("Moderator", `\`\`\`diff\n+ ${message.author.tag}!\`\`\``)
-            .setColor("#FF0000")
+            .setColor(config.embedcolor)
             banMember.send(userbanned).then(() =>
                 message.guild.members.ban(banMember, { days: 7, reason: reason })).catch(() => null)
             } catch {
@@ -54,14 +54,14 @@ module.exports = {
             }
             if (reason) {
             var sembed = new MessageEmbed()
-               .addField("Banned!", `\`\`\`diff\n+ ${banMember.user.tag} - [${banMember.user.id}] has been banned!\`\`\``)
+               .addField("Banned!", `\`\`\`diff\n+ ${banMember.user.tag} - [${banMember.user.id}] got banned!\`\`\``)
                .addField("Reason", `\`\`\`diff\n+ ${reason || "None."}!\`\`\``)
-              .setColor("#FF0000")
+            .setColor(config.embedcolor)
             message.channel.send(sembed)
             } else {
                 var sembed2 = new MessageEmbed()
-              .addField("Banned!", `\`\`\`diff\n+ ${banMember.user.tag} - [${banMember.user.id}] has been banned!\`\`\``)
-              .setColor("#FF0000")
+              .addField("Banned!", `\`\`\`diff\n+ ${banMember.user.tag} - [${banMember.user.id}] got banned!\`\`\``)
+            .setColor(config.embedcolor)
             message.channel.send(sembed2)
             }
     }
