d6165aa2799f7b58ef20830935a5a739d840b63f
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2021-01-09 14:01:17 +0530

    Add Moderation in command.js

--- handlers/command.js
+++ handlers/command.js
@@ -9,5 +9,5 @@ module.exports = (client) => {
             if (pull.config.aliases) pull.config.aliases.forEach(cmd => client.aliases.set(cmd, pull.config.name));
         };
     };
-    ["info", "nsfw", "anime", "image", "chatbot", "music", "fun", "other"].forEach(cmd => load(cmd));
+    ["info", "nsfw", "anime", "image", "chatbot", "music", "fun", "moderation", "other"].forEach(cmd => load(cmd));
 };
