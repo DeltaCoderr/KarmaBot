a0fb69d17ae2a80af261c1aec582eb1b91ccd931
AzrailDev <78249812+AzrailZeke@users.noreply.github.com>
2021-02-11 17:00:45 +0800

    💡 Added reason

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -12,16 +12,19 @@ module.exports = {
     run: async (bot, message, args) => {
      if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You need `Manage Channels` permission for this command!');
      
-     let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.guild.channels.cache.find(e => e.name === args[0]) || message.guild.channels.cache.find(e => e.name.startsWith(args[0]) || message.guild.channels.cache.find(p => p.name.endsWith(args[0])))
+    let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.guild.channels.cache.find(e => e.name === args[0]) || message.guild.channels.cache.find(e => e.name.startsWith(args[0]) || message.guild.channels.cache.find(p => p.name.endsWith(args[0])))
 
     if(!channel) return message.channel.send('Please provide a channel to lock!')
     
     if(channel.permissionsFor(message.guild.id).has("SEND_MESSAGES") === false) return message.channel.send("That channel is already locked.");
+        
+    let reason = args.slice(1).join(' ')
     
     try {
      await channel.updateOverwrite(message.guild.id, {
             SEND_MESSAGES: false
         });
+        channel.send(`${message.author.tag} locked this channel! ${reason? `\n\n**Reason**\n${reason}` : ''}`)
     message.channel.send(`<#${channel.id}> has been successfully locked.`)
         } catch(err) {
         console.log(err)
