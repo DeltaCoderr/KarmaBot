a593450fc003bcc6a981ba77ae2dc0fb117adc9a
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2021-02-14 22:41:49 +0530

    ❎ Remove NSFW Directory

--- handlers/command.js
+++ handlers/command.js
@@ -9,5 +9,5 @@ module.exports = (client) => {
             if (pull.config.aliases) pull.config.aliases.forEach(cmd => client.aliases.set(cmd, pull.config.name));
         };
     };
-    ["info", "nsfw", "anime", "image", "chatbot", "music", "fun", "other", "owner"].forEach(cmd => load(cmd));
+    ["info", "anime", "image", "chatbot", "music", "fun", "other", "owner"].forEach(cmd => load(cmd));
 };
