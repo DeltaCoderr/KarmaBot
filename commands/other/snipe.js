45af0b6f232456dbca59c3adb5a8184c71b355e2
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2021-03-12 21:56:55 +0530

    🔨  Update snipe.js

--- commands/other/snipe.js
+++ commands/other/snipe.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
-const db = require('quick.db')
+
 
 module.exports = {
     config: {
@@ -11,18 +11,31 @@ module.exports = {
         accessableby: "",
     },
     run: async (client, message, args) => {
-    
-        const msg = client.snipes.get(message.channel.id)
-        if(!msg) return message.channel.send("There is no deleted message!")
+
+       const msg = client.snipes.get(message.channel.id)
+        const embedn = new Discord.MessageEmbed()
+            .setAuthor(message.guild.name, message.guild.iconURL())
+            .setColor(config.embedcolor)
+            .setFooter(client.user.username, client.user.avatarURL())
+            .setDescription(`<@${message.author.id}> **There is no deleted messages.**`)
+            .setTimestamp();
+        if(!msg) return message.channel.send(embedn)
 
         const embed = new Discord.MessageEmbed()
-
-        .setAuthor(msg.author, message.author.displayAvatarURL({ dynamic: true }))
-        .addField('Content Of the Message :', msg.content)
+      
+        if(msg.content)
+        
+        embed.addField('Content Of the Message :', msg.content)
+        .setAuthor(msg.author, msg.authorimg)
         .setColor(config.embedcolor)
         .setFooter(" Requested by " + message.author.tag , message.author.avatarURL())
         .setTimestamp()
-        if(msg.image)embed.setImage(msg.image)
+
+        if(msg.image)
+        embed.setImage(msg.image)
+        .setColor(config.embedcolor)
+        .setDescription(`**Content of the message:**`)
+        .setAuthor(msg.author, msg.authorimg)
 
         message.channel.send(embed)
 
