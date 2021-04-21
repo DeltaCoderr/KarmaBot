869d956e60e5ad17ea4669397cc333301d0ff084
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 09:26:35 +0530

    Created Custom Embed Command

--- commands/Moderation/customembed.js
+++ commands/Moderation/customembed.js
@@ -3,50 +3,56 @@ const config = require('../../configs/config.json');
 
 
 module.exports = {
-    config: {
-        name: 'customembed',
-        description: 'Says your message through bot',
-        aliases: ["ce", "cembed"],
-        usage: '',
-        accessableby: "",
-    },
-    run: async (client, message, args) => {
-      if (!message.guild.me.hasPermission(["ADMINISTRATOR"]))
+  config: {
+    name: 'customembed',
+    description: 'Says your message through bot',
+    aliases: ["ce", "cembed"],
+    usage: '',
+    accessableby: "",
+  },
+  run: async (client, message, args) => {
+    if (!message.guild.me.hasPermission(["ADMINISTRATOR"]))
       return message.channel.send(
-        "I Don't Have Proper Permissions To Use This Command! \`${config.prefix}cembed <hex color> <title> <embed message>\`"
+        "I Don't Have Proper Permissions To Use This Command! \`d!cembed <hex color> <title> <embed message>\`"
       );
-        message.delete()
+    message.delete()
 
-        const color = args[0]
-        const title = args[1]
+    const color = args.slice(0).join(" ")
+    const title = args.slice(1).join(" ")
     const saymessage = args.slice(2).join(" ")
-    
-    if (color > 0) return message.channel.send({embed:{
-          color: config.embedcolor,
-          description:"Provide Hex Code for embed Color. \`${config.prefix}cembed <hex color> <title> <embed message>\`"}})
 
-    if (title > 1) return message.channel.send({embed:{
-          color: config.embedcolor,
-          description:"Provide Title for embed. \`${config.prefix}cembed <hex color> <title> <embed message>\`"}})
+    if (color < 0) return message.channel.send({
+      embed: {
+        color: config.embedcolor,
+        description: "Provide Hex Code for embed Color. \`${config.prefix}cembed <hex color> <title> <embed message>\`"
+      }
+    });
 
-    if (saymessage > 2) return message.channel.send({embed:{
-          color: config.embedcolor,
-          description:"Provide Message for Embed. \`${config.prefix}cembed <hex color> <title> <embed message>\`"}})
+    if (title < 1) return message.channel.send({
+      embed: {
+        color: config.embedcolor,
+        description: "Provide Title for embed. \`${config.prefix}cembed <hex color> <title> <embed message>\`"
+      }
+    });
+
+    if (saymessage < 2) return message.channel.send({
+      embed: {
+        color: config.embedcolor,
+        description: "Provide Message for Embed. \`${config.prefix}cembed <hex color> <title> <embed message>\`"
+      }
+    });
 
     const embed = new Discord.MessageEmbed()
+      .setTitle(title)
+      .setDescription(saymessage)
+      .setColor(color)
+      .setThumbnail(message.guild.iconURL())
+      .setFooter(message.author.tag, message.author.displayAvatarURL())
+      .setTimestamp();
+
+    return message.channel.send({ embed });
 
 
-    .setTitle(title)
 
-    .setDescription(saymessage)
-
-    .setColor(color)
-
-    .setThumbnail(message.guild.iconURL())
-
-    .setFooter(message.author.tag, message.author.displayAvatarURL())
-    .setTimestamp();
-
-    return message.channel.send(embed);
-      }
-    }
+  }
+}
