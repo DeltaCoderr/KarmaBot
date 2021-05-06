b81818806de1937d4832bae2277bcfbbb8a06447
DeltaCoderr <51528076+DeltaCoderr@users.noreply.github.com>
2021-05-06 17:11:48 +0530

    :hammer: Updated the trash command

--- commands/image/trash.js
+++ commands/image/trash.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
-const AmeClient = require('amethyste-api');
-const AmeAPI = new AmeClient(config.AME_API);
+const DIG = require("discord-image-generation")
 
 module.exports = {
     config: {
@@ -14,11 +13,17 @@ module.exports = {
     run: async (client, message, args) => {
 
         let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-        let m = await message.channel.send("**Please Wait...**");
-        let buffer = await AmeAPI.generate("trash", { url: user.user.displayAvatarURL({ format: "png", size: 2048 }) });
-        let attachment = new Discord.MessageAttachment(buffer, "trash.png");
-        m.delete({ timeout: 5000 });
-        message.channel.send(attachment);
+        let m = await message.channel.send("**Please Wait...**");   
+        let avatar = user.user.displayAvatarURL({
+             dynamic: false,
+             format: "png",
+           });
+       
+           let img = await new DIG.Trash().getImage(avatar);
+       
+           let attach = new Discord.MessageAttachment(img, "trash.png");
+           m.delete({ timeout: 5000 });
+           message.channel.send(attach);
 
     }
 }
