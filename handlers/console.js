23caa604aee2da53bd3c8a184f1c5adadfb7b462
Subodh Inamdar <51528076+DeltaCoderr@users.noreply.github.com>
2021-07-10 18:32:10 +0530

    Delete : Handlers Dir

--- handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, message) => {
-    let prompt = process.openStdin()
-    prompt.addListener('data', res => {
-        let console = res.toString().trim().split(/ +/g)
-        client.channels.cache.get(message.channel.id).send(x.join(" "))
-    });
-};
