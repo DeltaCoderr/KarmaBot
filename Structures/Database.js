e4cd706b39753d4646e0dde3be43c541357909b2
Madnesssik <78844616+Madnesssik@users.noreply.github.com>
2021-07-15 17:31:37 +0200

    ✨ Update : Little changes

--- Structures/Database.js
+++ Structures/Database.js
@@ -2,7 +2,7 @@ const { Database } = require('quickmongo');
 const db = new Database(config.database);
 
 db.on('ready', () => {
-    logger.success("[DATABASE]: Connected!");
+    logger.success("[DATABASE]: Connected !");
 });
 
 module.exports = { db };
