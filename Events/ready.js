19d9032fd6ab3803255d27a0b9ea38ee53acff60
Madnesssik <78844616+Madnesssik@users.noreply.github.com>
2021-07-15 17:37:43 +0200

    ✨ Update : Little Changes

--- Events/ready.js
+++ Events/ready.js
@@ -8,4 +8,6 @@ const { Bot } = require('../Structures/Client');
 module.exports = (client) => {
     
     logger.success(`[API]: Logged in as ${client.user.username}`);
+    logger.info(`[STATS]: Users -> ${client.guilds.cache.reduce((users, value) => users + value.memberCount, 0)}`);
+    logger.info(`[STATS]: Guilds -> ${client.guilds.cache.size}`);
 }
