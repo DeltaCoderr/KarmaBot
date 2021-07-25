5f01ebdd8e0a057ada8910aee7e23410c0f39b35
Madnesssik <78844616+Madnesssik@users.noreply.github.com>
2021-07-25 17:21:00 +0200

    cooldown

--- Events/message.js
+++ Events/message.js
@@ -13,12 +13,47 @@ module.exports = async (client, message) => {
 
 	if (!message.guild || message.author.bot) return;
 	const { config } = client;
+	const { cooldowns } = client;
 
 	if (message.content.startsWith(config.prefix)) {
 
 		let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 		let cmd = args.shift().toLowerCase();
+		const findcommand = client.commands.get(cmd);
+        	if(!findcommand) return;
 
+       		if(!cooldowns.has(findcommand.help.name)) {
+            	cooldowns.set(findcommand.help.name, new Collection());
+        	}
+		
+		const now = Date.now();
+       		const timestamps = cooldowns.get(findcommand.help.name);
+        	const cooldownAmount = (findcommand.help.cooldown || 3) * 1000;
+
+        	if(timestamps.has(message.author.id)) {
+            	const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+            	if(now < expirationTime) {
+                	const timeLeft = (expirationTime - now) / 1000;
+                	let hours   = Math.floor(timeLeft / 3600);
+               		let minutes = Math.floor((timeLeft - (hours * 3600)) / 60);
+               		let seconds = timeLeft - (hours * 3600) - (minutes * 60);
+               		if (hours   < 10) {hours   = "0"+hours;}
+               		if (minutes < 10) {minutes = "0"+minutes;}
+               		if (seconds < 10) {seconds = "0"+seconds;}
+               		const cooldownembed = new MessageEmbed()
+               			.setTitle('Cooldown Alert!')
+               			.setDescription(`Please wait **${hours}** Hours **${minutes}** Minutes **${seconds}** Seconds before reusing the \`${findcommand.help.name}\` command.`)
+               			.setColor(config.color)
+               			.setFooter(client.user.username, client.user.avatarURL())
+               			.setTimestamp();
+               		return message.reply(cooldownembed)
+          		}
+		}
+
+		timestamps.set(message.author.id, now);
+        	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+		
 		var commandFile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd))
 		if (commandFile) commandFile.run(client, message, args)
 	} else {
