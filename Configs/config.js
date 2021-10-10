bf978f2b3550ebd12894c56aecdf752733a2fe38
Subodh Inamdar <51528076+DeltaCoderr@users.noreply.github.com>
2021-10-10 19:21:06 +0530

    Update and rename config-example.js to config.js

--- Configs/config.js
+++ Configs/config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 	token: 'BOT_TOKEN', // Bot token
-	prefix: 'k!', // Prefix for commands
-	devs: '581752425858203659', // ID of the bot owner
+	prefix: '!', // Prefix for commands
+	devs: '', // ID of the bot owner
 	embedColor: '#5865F2', // Color of embeds
 	MongoURI: 'mongodb://localhost:27017/karma-bot', // Database URL
 	OSU_API_KEY: '', // Osu! API key
