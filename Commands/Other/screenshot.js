6d0b79f84a0cd660ac3f565748076684c44b8178
FarisDaffa <65797160+Faris0520@users.noreply.github.com>
2021-10-10 12:30:44 +0700

    Update screenshot.js

--- Commands/Other/screenshot.js
+++ Commands/Other/screenshot.js
@@ -13,6 +13,7 @@ module.exports = {
             const body = await fetch(`https://shot.screenshotapi.net/screenshot?&url=${encodeURIComponent(args.join(" "))}&fresh=true&output=json&file_type=png&wait_for_event=load`)
            .then(res => res.json().catch(() => { }));
 
+        const link = ["pornhub.com", "nhentai.net", "xnxx.com", ".xxx", "porn", "sex"]
             if (args.length === 0) return message.channel.send({
                embed: {
                     color: config.embedcolor,
@@ -31,7 +32,9 @@ module.exports = {
                     title: "❌ Error URL not found"
                 }
             });
-
+            if (message.content.toLowerCase().includes("pornhub.com") || message.content.toLowerCase().includes("nhentai.net") || message.content.toLowerCase().includes("xnxx.com") || message.content.toLowerCase().includes("xxx") || message.content.toLowerCase().includes("porn") || message.content.toLowerCase().includes("sex") || message.content.toLowerCase().includes("fuck")){
+                return message.channel.send("No NSFW please")
+            } else {
             const embed = new MessageEmbed()
                 .setTitle(`🌐 ${body.url} `)
                 .setColor(config.embedcolor)
@@ -39,5 +42,6 @@ module.exports = {
                 .setFooter(`© Karma `, "https://cdn.discordapp.com/attachments/725019921159028808/739770316754256012/Screenshot_20200803-1459592.png")
                 .setImage(`${body.screenshot}`)
             message.channel.send(embed);
+            }
         }
     }
