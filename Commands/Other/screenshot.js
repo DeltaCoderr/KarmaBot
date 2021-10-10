0801b7b478d394be4e89e3265b2ab1b7a3ef81b7
FarisDaffa <65797160+Faris0520@users.noreply.github.com>
2021-10-10 12:38:54 +0700

    Create screenshot.js
    idk this commit work or not

--- Commands/Other/screenshot.js
+++ Commands/Other/screenshot.js
@@ -9,11 +9,14 @@ module.exports = {
         category: __dirname.split("Commands\\")[1]
     },
     run: async (client, message, args) => {
-
+            
+            let argz = args.join(" ")
+            if (argz.toLowerCase().includes("pornhub.com") || argz.toLowerCase().includes("nhentai.net") || argz.toLowerCase().includes("xnxx.com") || argz.toLowerCase().includes("xxx") || argz.toLowerCase().includes("porn") || argz.toLowerCase().includes("sex") || argz.toLowerCase().includes("fuck"))
+                return message.channel.send("No NSFW please")
+        
             const body = await fetch(`https://shot.screenshotapi.net/screenshot?&url=${encodeURIComponent(args.join(" "))}&fresh=true&output=json&file_type=png&wait_for_event=load`)
            .then(res => res.json().catch(() => { }));
 
-        const link = ["pornhub.com", "nhentai.net", "xnxx.com", ".xxx", "porn", "sex"]
             if (args.length === 0) return message.channel.send({
                embed: {
                     color: config.embedcolor,
@@ -32,10 +35,7 @@ module.exports = {
                     title: "❌ Error URL not found"
                 }
             });
-            let argz = args.join(" ")
-            if (argz.toLowerCase().includes("pornhub.com") || argz.toLowerCase().includes("nhentai.net") || argz.toLowerCase().includes("xnxx.com") || argz.toLowerCase().includes("xxx") || argz.toLowerCase().includes("porn") || argz.toLowerCase().includes("sex") || argz.toLowerCase().includes("fuck")){
-                return message.channel.send("No NSFW please")
-            } else {
+            
             const embed = new MessageEmbed()
                 .setTitle(`🌐 ${body.url} `)
                 .setColor(config.embedcolor)
