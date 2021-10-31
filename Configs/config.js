2ca3e1f43d776b9d8c77b7b84a8d5077ea0559d4
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2021-10-31 18:28:45 +1000

    Updated config.json.

--- Configs/config.js
+++ Configs/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: 'OTA0MjY2NTMxNTcwMDU3MjQ3.YX5B1Q.K17H85XFAlN97uL5ul0hh_5xbiE',
-    prefix: 'p!',
-    devs: 'Anaxes',
-    embedcolor: '00c8ff',
-    database: 'mongodb+srv://Anaxes:ehEFFJ2IbsgEoszg@thing.yhyau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    FACT_API: '3d47d1f87c6525345787f6f7f91e07b6f5efa07608cd',
-    AME_API: 'b31f5ddda10d956ab4371edbf107cf2930babc5d89f82058fb53619c35a00520fa7e416663481c18ab4bdc69fde7492f8cef7d4d94188c50c08f54e32a2f991d'
+    token: '',
+    prefix: '',
+    devs: '',
+    embedcolor: '',
+    database: '',
+    FACT_API: '',
+    AME_API: ''
 }
