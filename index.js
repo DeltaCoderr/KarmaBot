b3001e7cf0280a67f9e5d297f265b584490f7b2a
Nin <darshan310268@gmail.com>
2022-03-10 11:26:40 +0530

    Deleted Files

--- index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const KarmaClient = require("./Library/KarmaClient");
-
-const client = new KarmaClient({
-	intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
-});
-
-(async () => await client.start(process.env.TOKEN))();
