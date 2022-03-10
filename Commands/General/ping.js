914926493115b6c97ed6a2dcb9aa201127991fb4
Nin <darshan310268@gmail.com>
2022-03-10 11:27:24 +0530

    feat: test commands

--- Commands/General/ping.js
+++ Commands/General/ping.js
@@ -1,26 +1,14 @@
-const KarmaCommand = require("../../Library/KarmaCommand");
-const { Message } = require("discord.js");
+const Command = require("../../Client/Command");
 
-module.exports = class PingCommand extends KarmaCommand {
-	constructor(context, options) {
-		super(context, {
-			...options,
-			aliases: ["pong"],
-			description: "Latency of this Bot",
-		});
-	}
-	/**
-	 * @param {Message} msg
-	 */
-	async messageRun(msg) {
-		const initial = await msg.reply(`Ping?`).catch(() => {});
-
-		await initial
-			.edit(
-				`Pong! Discord: ${
-					initial.createdTimestamp - msg.createdTimestamp
-				} ms. Websocket: ${this.container.client.ws.ping} ms.`
-			)
-			.catch(() => {});
-	}
-};
+module.exports = new Command({
+	name: "ping",
+	aliases: ["pong"],
+	description: "The ping of the bot",
+	category: "General",
+	run: async (client, message, args) => {
+		message.reply(`Pong!・\`${client.ws.ping}\` ms.`).catch(() => {});
+	},
+	exec: async (client, interaction) => {
+		interaction.reply(`Pong!・\`${client.ws.ping}\` ms.`).catch(() => {});
+	},
+});
