8b0d79a0fdb260c6523804f408cdca18e3af3b31
Nin <darshan310268@gmail.com>
2022-03-22 19:55:34 +0530

    **untouched**

---
+++ player-events/searchCancel.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+
+module.exports = (client, message) => {
+	const embed = new Discord.MessageEmbed()
+	.setTitle('Search Cancelled!')
+	.setDescription(`${emotes.error} - You did not provide a valid response ... Please send the command again !`)
+	.setFooter('Karma Music System')
+	.setColor(config.embedcolor)
+	.setTimestamp();
+    message.channel.send(embed);
+
+};
---
+++ player-events/trackAdd.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+
+module.exports = (client, message, track) => {
+	const embed = new Discord.MessageEmbed()
+	.setTitle('Track Added!')
+	.setDescription(`${emotes.music} - ${track.title} has been added to the queue !`)
+	.setFooter('Karma Music System')
+	.setColor(config.embedcolor)
+	.setTimestamp();
+    message.channel.send(embed);
+
+};
