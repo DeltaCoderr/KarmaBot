6567152c79c57a2439d84fa5ea9bee126049b8de
Nin <darshan310268@gmail.com>
2022-03-22 19:56:45 +0530

    Deletion

--- old/Events/guildCreate.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, guild) => {
-	console.log(`Fetching ${guild.name} members`);
-	await guild.members.fetch();
-	console.log(`Fetched ${guild.name} members`);
-};
--- old/index.js
+++
@@ -1,6 +0,0 @@
-const { Karma } = require('./Structures/Client');
-const client = new Karma();
-
-client.snipes = new Map();
-
-client.start();
