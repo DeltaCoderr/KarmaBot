6a24f317001ed72f7808b8ac3e446fa9149a2fb0
Nin <darshan310268@gmail.com>
2022-03-22 19:54:15 +0530

    chore: global vars

--- Utils/Global.js
+++ Utils/Global.js
@@ -1,2 +1,11 @@
-global.Command = require("../Client/Command");
-global.Listener = require("../Client/Listener");
+global.toID = function (text) {
+	if (typeof text === "string")
+		return text.toLowerCase().replace(/[^a-z0-9]/g, "");
+};
+
+global.config = require("../Configs/config");
+global.emotes = require("../Configs/emotes");
+global.filters = require("../Configs/filters");
+global.Command = require("../Structures/Command");
+global.Listener = require("../Structures/Listener");
+global.KarmaBot = require("../Structures/Client");
