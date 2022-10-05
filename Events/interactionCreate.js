201ba6594dd4a15f05619d985199612769968249
Yash Patil <yashpatil.yp2711@gmail.com>
2022-10-05 12:30:35 +0530

    changes in events folder

---
+++ Events/interactionCreate.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'ready',
+    async execute (interaction, client) {
+      if (!interaction.isCommand()) return;
+      
+      const { commandName } = interaction;
+      const { commands } = client;
+      const command = commands.get(commandName);
+      
+      if (!command) return;
+    
+      try {
+        await command.execute(interaction, client);
+      }
+      catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+  }
+}
--- Events/ready.js
+++ Events/ready.js
@@ -3,6 +3,10 @@ const { Bot } = require('../Structures/Client');
  * @param { Bot } client
  */
 
-module.exports = (client) => {
-    console.log(`[API] : Logged in as ${client.user.username}`);
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(interaction, client) {
+        console.log(`[API] : Logged in as ${client.user.username}`);
+    }
 }
