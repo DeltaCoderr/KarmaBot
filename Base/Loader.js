49a3484abf5c71358189ac400d760a281d108e30
Latesh Sharma <121330785+latesh-11@users.noreply.github.com>
2023-10-31 23:22:38 +0530

    Refactor command and event loader for improved readability
    - Use `path` module for handling file paths.
    - Replace `fs.readdirSync` with `fs.promises.readdir` for asynchronous directory reading.
    - Improved error handling for event loading.
    - Check for the presence of `help` and `aliases` properties before using them.

--- Base/Loader.js
+++ Base/Loader.js
@@ -1,34 +1,47 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = {
-	LoadCommands: async function(client) {
-		fs.readdirSync(`${__dirname}/../Commands`).forEach((dir) => {
-			const commands = fs.readdirSync(`${__dirname}/../Commands\\${dir}`);
-			commands.forEach((file) => {
-				const pull = require(`${__dirname}/../Commands\\${dir}/${file}`);
-				if (pull.help.name) {
-					client.commands.set(pull.help.name, pull);
-					pull.help.aliases.forEach((alias) => {
-						client.aliases.set(alias, pull.help.name);
-					});
-				}
-			});
-		});
-		console.log(`Successfully loaded ${client.commands.size} commands.`);
-	},
-	LoadEvents: async function(client) {
-		fs.readdir(`${__dirname}/../Events`, async (err, files) => {
-			if (err) {
-				return console.log(
-					`An Error Occcured While Loading Events. ${err.stack}`,
-				);
-			}
-			for (let i = 0; i < files.length; i++) {
-				const event = require(`../Events/${files[i]}`);
-				const eventName = files[i].split('.')[0];
-				client.on(eventName, event.bind(null, client));
-			}
-			console.log(`Successfully loaded ${files.length} events.`);
-		});
-	},
+  loadCommands: async function (client) {
+    const commandsDir = path.join(__dirname, '../Commands');
+
+    // Use async readdir to make it non-blocking
+    const dirContents = await fs.promises.readdir(commandsDir);
+
+    for (const dir of dirContents) {
+      const dirPath = path.join(commandsDir, dir);
+      const commands = await fs.promises.readdir(dirPath);
+
+      for (const file of commands) {
+        const filePath = path.join(dirPath, file);
+        const pull = require(filePath);
+
+        if (pull.help && pull.help.name) {
+          client.commands.set(pull.help.name, pull);
+
+          if (pull.help.aliases && Array.isArray(pull.help.aliases)) {
+            pull.help.aliases.forEach((alias) => {
+              client.aliases.set(alias, pull.help.name);
+            });
+          }
+        }
+      }
+    }
+
+    console.log(`Successfully loaded ${client.commands.size} commands.`);
+  },
+
+  loadEvents: async function (client) {
+    const eventsDir = path.join(__dirname, '../Events');
+    const eventFiles = await fs.promises.readdir(eventsDir);
+
+    for (const file of eventFiles) {
+      const eventName = path.parse(file).name;
+      const event = require(path.join(eventsDir, file));
+
+      client.on(eventName, (eventHandler) => event(eventHandler, client));
+    }
+
+    console.log(`Successfully loaded ${eventFiles.length} events.`);
+  },
 };
