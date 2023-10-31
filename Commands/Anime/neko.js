b15e5e1b0060afc2fe0f708d01730c8c82a0c642
Devendra Parihar <54232149+Devparihar5@users.noreply.github.com>
2023-10-31 22:00:55 +0530

    Optimize and standardize 'neko' command
    - Encapsulated image fetching and embed creation in separate functions.
    - Added try...catch for error handling during image fetching.
    - Removed dependency on 'config' for the embed color.
    - Improved error handling with informative error messages.
    - Enhanced code readability with comments and organization.
    - Corrected the category determination using '__filename.split'.

--- Commands/Anime/neko.js
+++ Commands/Anime/neko.js
@@ -1,27 +1,41 @@
 const undici = require('undici');
-const Discord = require('discord.js');
-
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	help: {
-		name: 'neko',
-		aliases: [],
-		description: 'Drops random Neko Pictures.',
-		category: __dirname.split('Commands\\')[1],
-	},
-	run: async (client, message) => {
-		const { url } = await undici.fetch('https://shiro.gg/api/images/neko').then((res) => res.json());
-
-		const embed = new Discord.MessageEmbed()
-			.setTitle('Here\'s your Neko')
-			.setImage(url)
-			.setColor(config.embedColor)
-			.setTimestamp()
-			.setFooter(
-				'© Karma',
-				'https://i.imgur.com/U34MPtp.png',
-			)
-			.setURL(url);
-		message.reply({ embeds: [embed] });
-	},
+  help: {
+    name: 'neko',
+    aliases: [],
+    description: 'Drops random Neko Pictures.',
+    category: __filename.split('Commands/')[1],
+  },
+  run: async (client, message) => {
+    try {
+      const { url } = await fetchNekoImage();
+      const embed = createNekoEmbed(url);
+      message.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error('Error while fetching or sending neko image:', error);
+      message.reply('An error occurred while fetching the neko image.');
+    }
+  },
 };
+
+async function fetchNekoImage() {
+  const response = await undici.fetch('https://shiro.gg/api/images/neko');
+  if (response.statusCode === 200) {
+    const { url } = await response.json();
+    return { url };
+  } else {
+    throw new Error(`Failed to fetch neko image. Status code: ${response.statusCode}`);
+  }
+}
+
+function createNekoEmbed(url) {
+  return new MessageEmbed()
+    .setTitle("Here's your Neko")
+    .setImage(url)
+    .setColor('#yourEmbedColor') // Replace with your desired embed color
+    .setTimestamp()
+    .setFooter('© Karma', 'https://i.imgur.com/U34MPtp.png')
+    .setURL(url);
+}
